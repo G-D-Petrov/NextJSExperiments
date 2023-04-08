12d56ef8a447e7d7faa9f925ba615f68abea4c59
Georgi Petrov <petrov.d.george@gmail.com>
2023-04-08 14:08:54 +0300

    Add rate limiting

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,5 +1,9 @@
 import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
+
 import { z } from "zod";
 
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
@@ -11,6 +15,19 @@ const filterUserForClient = (user: User) => {
         profileImageUrl: user.profileImageUrl,
     };
 };    
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */ 
+  prefix: "@upstash/ratelimit",
+});
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query( async ({ ctx }) => {
@@ -38,6 +55,16 @@ export const postsRouter = createTRPCRouter({
         content: z.string().min(1).max(250),
     })).mutation(async ({ ctx, input }) => {
         const authorId = ctx.userId;
+
+        const { success } = await ratelimit.limit(authorId);
+
+        if (!success) {
+            throw new TRPCError({
+                code: "TOO_MANY_REQUESTS",
+                message: "You have reached the limit of 3 posts per minute",
+            });
+        }
+
         const post = await ctx.prisma.post.create({
             data: {
                 authorId,
